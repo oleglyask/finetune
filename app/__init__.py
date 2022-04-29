44d7490384fc52648d29df5a195d9035c428d35b
oleglyask <olegly@outlook.com>
2022-04-29 09:48:01 -0700

    js bug

--- app/__init__.py
+++ app/__init__.py
@@ -56,15 +56,15 @@ def create_app(config_name='default'):
                 'default-src': [
                     "'self'",
                     'cdnjs.cloudflare.com',
-                    'cdn.jsdelivr.net',
+                    'cdn.jsdelivr.net'
                 ],
                 # allow inline script source.  Must add <script nonce="{{ csp_nonce() }}"> to html tag
-                # 'script-src': '\'self\'',
+                'script-src': "'self'",
                 # allow images from anywhere,
                 #   including unicornify.pictures
                 'img-src': '*'
             },
-            # content_security_policy_nonce_in=['script-src']
+            content_security_policy_nonce_in=['script-src']
         )
     #################################################################
 

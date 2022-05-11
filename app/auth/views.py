453ff3a5a7e125622c33945a3a88133fcf2a39db
oleglyask <olegly@outlook.com>
2022-05-11 16:42:42 -0700

    final details

--- app/auth/views.py
+++ app/auth/views.py
@@ -88,9 +88,7 @@ def change_email():
         db.session.add(current_user._get_current_object())
         db.session.commit()
         flash('You have successfully changed your email')
-        # return redirect(url_for('main.home'))
-        # return render_template('auth/email/confirm.html')
-        return redirect(url_for('auth.resend_confirmation'))
+        return redirect(url_for('main.home'))
     return render_template('auth/auth_forms.html', form=form, title='change email')
 
 # Will take a token, attempt to confirm the user, then redirect to the index page
